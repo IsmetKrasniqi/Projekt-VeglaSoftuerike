7bf6e996320b38b483ecaace2002a11c7dc58e49
IsmetKrasniqi <ismet.krasniqi1@student.uni-pr.edu>
2026-01-02 16:41:15 +0100

    Add main function and input handling

--- main.cpp
+++ main.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include "paga.h"
+
+using namespace std;
+
+int main()
+{
+    int nrOreveTePunes;
+    float paga;
+
+    cout << "Shenoni numrin e oreve qe keni punuar ne kete muaj: ";
+    cin >> nrOreveTePunes;
+
+    cout << "Shenoni pagen mujore: ";
+    cin >> paga;
+
+    float pagaZbritur = llogaritZbritjen(nrOreveTePunes, paga);
+    shfaqMesazhin(nrOreveTePunes);
+    shfaqRezultatin(paga, pagaZbritur);
+
+    return 0;
+}
 
