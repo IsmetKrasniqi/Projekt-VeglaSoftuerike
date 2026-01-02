0be2bbff262a42d94108568a6048da0cd8263c71
IsmetKrasniqi <ismet.krasniqi1@student.uni-pr.edu>
2026-01-02 17:09:34 +0100

    Header file paga.h me deklarime te perbashketa

---
+++ paga.h
@@ -0,0 +1,10 @@
+#ifndef PAGA_H
+#define PAGA_H
+
+extern const int MAKS_ORET_NE_MUAJ;
+
+float llogaritZbritjen(int ore, float paga);
+void shfaqMesazhin(int ore);
+void shfaqRezultatin(float paga, float pagaZbritur);
+
+#endif
