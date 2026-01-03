09f44486bb385e3cfc8ddbc97f9a30fb5f93febf
erdirexha1 <erdi.rexha1@student.uni-pr.edu>
2026-01-03 02:56:31 +0100

    Update LlogaritOretShtese.cpp
    Në këtë commit kam realizuar bashkimin e funksionit të bonusit me funksionin e shfaqjes së mesazheve. Tani sistemi nuk tregon vetëm orët e tejkaluara, por llogarit automatikisht edhe shumën monetare që i takon punonjësit si bonus. Gjithashtu, kam përditësuar tarifën e orës shtese.

--- LlogaritOretShtese.cpp
+++ LlogaritOretShtese.cpp
@@ -3,25 +3,27 @@
 
 using namespace std;
 
-void shfaqMesazhin(int ore)
-{
-if (ore > MAKS_ORET_NE_MUAJ) {
-cout << "Ju keni tejkaluar numrin e oreve per "
-<< ore - MAKS_ORET_NE_MUAJ << " ore." << endl;
-}
-else if (ore == MAKS_ORET_NE_MUAJ) {
-cout << "Ju keni realizuar oret sipas parashikimit." << endl;
-}
-else {
-cout << "Ju keni "
-<< MAKS_ORET_NE_MUAJ - ore
-<< " ore me pak se plani mujor." << endl;
-}
-}
+// Ky eshte funksioni per llogaritjen e parave shtese (bonusi)
 double llogaritBonusin(int ore) {
     const double PAGESA_PER_ORE_SHTESE = 10.5; 
     if (ore > MAKS_ORET_NE_MUAJ) {
         return (ore - MAKS_ORET_NE_MUAJ) * PAGESA_PER_ORE_SHTESE;
     }
     return 0.0;
+}
+
+
+void shfaqMesazhin(int ore) {
+    if (ore > MAKS_ORET_NE_MUAJ) {
+        // Ketu therrasim llogaritjen e bonusit qe shtuam me larte
+        double bonusi = llogaritBonusin(ore);
+        cout << "Ju keni tejkaluar numrin e oreve per " << ore - MAKS_ORET_NE_MUAJ << " ore." << endl;
+        cout << "Bonusi juaj eshte: " << bonusi << " Euro." << endl;
+    } 
+    else if (ore == MAKS_ORET_NE_MUAJ) {
+        cout << "Ju keni realizuar oret sipas parashikimit." << endl;
+    } 
+    else {
+        cout << "Ju keni " << MAKS_ORET_NE_MUAJ - ore << " ore me pak se plani mujor." << endl;
+    }
 }
