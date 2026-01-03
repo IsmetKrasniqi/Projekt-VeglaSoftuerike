93012a94dd02eb6956b6a81fb00f40b19f8b5609
ensartara <ensar.tara1@student.uni-pr.edu>
2026-01-03 16:55:54 +0100

    Update rregullat.cpp
    Nese perdoruesi jep ore ose page negative ndalet ekzekutimi programit

--- rregullat.cpp
+++ rregullat.cpp
@@ -4,6 +4,9 @@ const int MAKS_ORET_NE_MUAJ = 176;
 
 float llogaritZbritjen(int ore, float paga)
 {
+    if (ore < 0 || paga < 0) {
+        return 0;
+    }
     if (ore < MAKS_ORET_NE_MUAJ) {
         float perqindjaRealizimit = 1 - (ore / (float)MAKS_ORET_NE_MUAJ);
         return perqindjaRealizimit * paga;
